d470dd2e8ab5d9c6e75e9579fc293c1dd4b9a34f
Jeffrey Liu <jeffreyliu8@gmail.com>
2020-06-02 16:56:36 -0700

    android studio 4.0 gradle update

--- app/src/main/java/com/askjeffreyliu/myapplication/MainActivity.java
+++ app/src/main/java/com/askjeffreyliu/myapplication/MainActivity.java
@@ -3,7 +3,7 @@ package com.askjeffreyliu.myapplication;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 
 import com.askjeffreyliu.floydsteinbergdithering.Utils;
